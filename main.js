b3fa1b66eaa721e746ef181678c2b97636285394
Leire Ordeñana <leireomadina@gmail.com>
2020-10-13 09:16:32 +0200

    Get html elements in JS

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+"use strict";
+
+//tests if the js file works
 // alert ("Hola");
+
+//get html elements
+const number = document.querySelector(".js-number");
+console.log(number);
+const btn = document.querySelector(".js-btn");
+console.log(btn);
+const hint = document.querySelector(".js-hint");
+console.log(hint);
+const attemps = document.querySelector(".js-attemps");
+console.log(attemps);
+

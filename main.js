9175ac9eac3be5fb9bb9b13de833fe85ad096602
Leire Ordeñana <leireomadina@gmail.com>
2020-10-13 09:19:49 +0200

    Add random number function

--- main.js
+++ main.js
@@ -5,11 +5,17 @@
 
 //get html elements
 const number = document.querySelector(".js-number");
-console.log(number);
+// console.log(number);
 const btn = document.querySelector(".js-btn");
-console.log(btn);
+// console.log(btn);
 const hint = document.querySelector(".js-hint");
-console.log(hint);
+// console.log(hint);
 const attemps = document.querySelector(".js-attemps");
-console.log(attemps);
+// console.log(attemps);
 
+//creates a random number
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+console.log(getRandomNumber(100));

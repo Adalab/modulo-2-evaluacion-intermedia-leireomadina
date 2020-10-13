53ab76b3299549e832727e9acfe80c5f860e0aa9
Leire Ordeñana <leireomadina@gmail.com>
2020-10-13 10:14:05 +0200

    Add attemps function and finish JS

--- main.js
+++ main.js
@@ -5,29 +5,33 @@ const number = document.querySelector(".js-number");
 const btn = document.querySelector(".js-btn");
 const hint = document.querySelector(".js-hint");
 const attemps = document.querySelector(".js-attemps");
-
-//generates a random number between 0 and 100 each times the page is refreshed
+//generates a random number between 0 and 100 each time the page loads
 const randomNumber = getRandomNumber(100);
-//gets the input value
-// let selectedNumber;
+//sets the accumulator initial count to 0
+let acc = 0;
 
 //creates a random number
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-
 //prints the random number in the console
 console.log(`Mi número aleatorio es ${randomNumber}`);
-//checks data type: it is a number
+//checks data type: it is already a number :)
 // console.log(typeof(randomNumber));
 
+//updates the attemps
+function updateAttemp () {
+  //adding 1 to the accumulator each time
+  acc += 1;
+  attemps.innerHTML = `Número de intentos: ${acc}`;
+}
 
 //handler fot the btn listener
 function updateClick () {
   //gets the input value and converts it to a number data type
   let selectedNumber = parseInt(number.value);
   console.log(`El número seleccionado por el usuario es ${selectedNumber}`);
-  //checks data type: it is a string
+  //checks data type: converted to number :D
   // console.log(typeof(selectedNumber));
   if (selectedNumber === randomNumber) {
     hint.innerHTML = "¡¡¡Has ganado campeona!!!";
@@ -40,12 +44,9 @@ function updateClick () {
   } else {
     hint.innerHTML = "Tienes que escribir antes un número";
   }
+  //updates the attemps each time the button is clicked
+  updateAttemp();
 }
 
-//does not work ftm :(
-// function convertToNumber (string) {
-//   return parseInt(string);
-// }
-
-//listener
+//listener for the button
 btn.addEventListener("click", updateClick);

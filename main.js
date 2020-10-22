50b5016cfdb1d29ad2aea1f52fa552a7e0a1f31c
Leire Ordeñana <leireomadina@gmail.com>
2020-10-22 18:39:15 +0200

    Create new function to improve code organization

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 "use strict";
 
-//get html elements
 const number = document.querySelector(".js-number");
 const btn = document.querySelector(".js-btn");
 const hint = document.querySelector(".js-hint");
 const attemps = document.querySelector(".js-attemps");
-const reset = document.querySelector('.js-reset');
+const reset = document.querySelector(".js-reset");
 //generates a random number between 0 and 100 each time the page reloads
 const randomNumber = getRandomNumber(100);
 //sets the accumulator initial count to 0
@@ -15,21 +14,17 @@ let acc = 0;
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-//prints the random number in the console
 console.log(`Mi número aleatorio es ${randomNumber}`);
 
-//updates the attemps
-function updateAttemp () {
+function updateAttemp() {
   //adding 1 to the accumulator each time
   acc += 1;
   attemps.innerHTML = `Número de intentos: ${acc}`;
 }
 
-//handler fot the btn listener
-function updateClick () {
+function updateClue() {
   //gets the input value and converts it to a number data type
   let selectedNumber = parseInt(number.value);
-  //conditional statements:
   if (selectedNumber === randomNumber) {
     renderHint("¡¡¡Has ganado campeona!!!");
   } else if (selectedNumber <= 0 || selectedNumber > 100) {
@@ -41,8 +36,6 @@ function updateClick () {
   } else {
     renderHint("Pista: escribe primero un número :)");
   }
-  //updates the attemps each time the button is clicked
-  updateAttemp();
 }
 
 //to render the hint messages within the conditional statements
@@ -50,8 +43,14 @@ function renderHint(string) {
   hint.innerHTML = string;
 }
 
+//handler fot the btn listener
+function updateClick() {
+  updateClue();
+  updateAttemp();
+}
+
 //reset attemps: sets the accumulator initial count to 0 again
-function resetBtn () {
+function resetBtn() {
   acc = 0;
   attemps.innerHTML = `Número de intentos: ${acc}`;
 }
